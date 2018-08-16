04e141be7e7081dedf79db1879b352bd48c856cf
Janghoon Lee <chaossky@naver.com>
2018-08-17 07:31:41 +0900

    j를 초기화시켜야 된자든 것을 깨달으시요.

---
+++ Project1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int i = 2;
+
+	while (i < 10) {
+		int j = 1;
+		while (j < 10) {
+			cout << i << " X " << j << " = " << i * j << '\t';
+
+			j++;
+		}
+		cout << endl;
+		i++;
+	}
+	
+
+
+
+	/*cout << "Hello" << endl;*/
+	return 0;
+}
